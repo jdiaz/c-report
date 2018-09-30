ab5e44873513781ff624cb78e9b9074a9bae2979
jdiaz <jrd.palacios@gmail.com>
2018-09-29 20:33:22 -0500

    Inital commit

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+// crypto report
+
+import (
+  "path/filepath"
+  "os"
+  "flag"
+  "fmt"
+  "strings"
+  "io/ioutil"
+)
+
+func visit(path string, f os.FileInfo, err error) error {
+  parts := strings.Split(path, ".")
+  if len(parts) != 2 {
+    return nil
+  }
+  if parts[1] == "crypto" {
+    fmt.Printf("File match in: %s\n", path)    
+  }
+  return nil
+} 
+
+
+func main() {
+  file, err := ioutil.ReadFile("banner.txt")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  banner := string(file)
+  fmt.Println(banner)
+  fmt.Printf("Searching for .crypto files...\n")
+  flag.Parse()
+  root := flag.Arg(0)
+  filepath.Walk(root, visit)
+  fmt.Println("Search complete.")
+}
