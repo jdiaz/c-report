425ffc9165a258ee49dedd30efb0ad024755f9a0
jdiaz <jrd.palacios@gmail.com>
2018-10-28 13:13:26 -0500

    Ran go fmt

--- main.go
+++ main.go
@@ -1,39 +1,36 @@
 package main
 
-// crypto report
-
 import (
-  "path/filepath"
-  "os"
-  "flag"
-  "fmt"
-  "strings"
-  "io/ioutil"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func visit(path string, f os.FileInfo, err error) error {
-  parts := strings.Split(path, ".")
-  if len(parts) != 2 {
-    return nil
-  }
-  if parts[1] == "crypto" {
-    fmt.Printf("File match in: %s\n", path)    
-  }
-  return nil
-} 
-
+	parts := strings.Split(path, ".")
+	if len(parts) != 2 {
+		return nil
+	}
+	if parts[1] == "crypto" {
+		fmt.Printf("File match in: %s\n", path)
+	}
+	return nil
+}
 
 func main() {
-  file, err := ioutil.ReadFile("banner.txt")
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  banner := string(file)
-  fmt.Println(banner)
-  fmt.Printf("Searching for .crypto files...\n")
-  flag.Parse()
-  root := flag.Arg(0)
-  filepath.Walk(root, visit)
-  fmt.Println("Search complete.")
+	file, err := ioutil.ReadFile("banner.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	banner := string(file)
+	fmt.Println(banner)
+	fmt.Printf("Searching for .crypto files...\n")
+	flag.Parse()
+	root := flag.Arg(0)
+	filepath.Walk(root, visit)
+	fmt.Println("Search complete.")
 }
